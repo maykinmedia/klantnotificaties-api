213bb7cd3972c554510d2c2d2aa0e0ac49a08ee9
Steven Bal <steven@maykinmedia.nl>
2021-07-13 16:37:15 +0200

    :recycle: Update compile_depencies scripts

--- bin/compile_dependencies.sh
+++ bin/compile_dependencies.sh
@@ -1,4 +1,17 @@
+
 #!/bin/bash
+
+#
+# Compile the dependencies for production, CI and development.
+#
+# Usage, in the root of the project:
+#
+#     ./bin/compile_dependencies.sh
+#
+# Any extra flags/arguments passed to this wrapper script are passed down to pip-compile.
+# E.g. to update a package:
+#
+#     ./bin/compile_dependencies.sh --upgrade-package django
 
 set -ex
 
@@ -6,24 +19,26 @@ toplevel=$(git rev-parse --show-toplevel)
 
 cd $toplevel
 
-# Base deps
+export CUSTOM_COMPILE_COMMAND="./bin/compile_dependencies.sh"
+
+# Base (& prod) deps
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    "$@" \
     requirements/base.in
 
-# Dev deps
+# Dependencies for testing
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    --output-file requirements/ci.txt \
+    "$@" \
+    requirements/base.txt \
+    requirements/test-tools.in
+
+# Dev depedencies - exact same set as CI + some extra tooling
+pip-compile \
+    --no-emit-index-url \
     --output-file requirements/dev.txt \
-    requirements/base.txt \
-    requirements/testing.in \
+    "$@" \
+    requirements/ci.txt \
     requirements/dev.in
-
-
-# Jenkins/tests deps
-pip-compile \
-    --no-index \
-    --output-file requirements/jenkins.txt \
-    requirements/base.txt \
-    requirements/dev.txt \
-    requirements/jenkins.in
